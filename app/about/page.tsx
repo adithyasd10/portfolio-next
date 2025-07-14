e5172fae2e758281b30896ac1c37f85edf0a3f85
Adithya S D <adithyasd@gmail.com>
2025-07-14 10:19:36 +0530

    About section created

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Aboutpage = () => {
+  return (
+    <div className='flex flex-col justify-start text-start p-40'>
+        <div className='flex flex-col gap-4'>
+            <h1 className='flex text-3xl md:text-6xl text-start bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-blue-500 to-blue-700 font-extrabold hover:from-blue-200 hover:via-blue-400 hover:to-blue-700'>ABOUT</h1>
+            <p className='text-sm sm:text-xl md:text-2xl  text-gray-500'>Computer engineering student at Marian Engineering College with a strong interest in full-stack and mobile app development. Experienced in building responsive web apps using HTML, CSS, SCSS, TypeScript,Next.js,React and cross-platform mobile apps with Flutter, Android Studio, and Flask.Skilled in C, Data Structures, and Python, with a passion for solving real-world problems through scalable tech solutions.Open to internships and entry-level roles in software/mobile development</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Aboutpage
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 const Navbar = () => {
   return (
     <main className="flex justify-center p-4">
-      <nav className="flex justify-around items-center p-4 w-sm bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 flex-row border-b border-black rounded-4xl text-black ">
-        <Link href="/about" className="hover:text-gray-400">
+      <nav className="flex justify-around items-center p-4 w-sm bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 flex-row border-b border-black rounded-4xl text-black backdrop-blur-md">
+        <Link href="/about" className="hover:text-blue-700">
           <button>About</button>
         </Link>
-        <Link href="/projects" className="hover:text-gray-400">
+        <Link href="/projects" className="hover:text-blue-700">
          <button>Projects</button>
         </Link>
-        <Link href="/contacts" className="hover:text-gray-400">
+        <Link href="/contacts" className="hover:text-blue-700">
           <button>Contacts</button>
         </Link>
       </nav>
