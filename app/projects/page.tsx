a0dfb57ff2d3df874424c09dfe3efcfe1e58e64b
Adithya S D <adithyasd@gmail.com>
2025-07-17 20:32:41 +0530

    Added completed project cards and styled project section layout

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,13 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex flex-col bg-gradient-to-r from-blue-200 via-blue-300 to-blue-700 min-h-screen p-4 justify-center items-center'>
-        <Cards title="Movie Reviews App" description="A modern movie app project built with tools like Next.js 14, Tailwind CSS, and the TMDB API. This project helps you learn how to create a responsive, interactive, and visually stunning movie browsing experience, similar to IMDB." image="/popcornflix1.png" link="/"/>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-7 p-4 m-2 transition-all duration-700'>
+        <Cards title="Movie Reviews App" description="A modern movie app project built with tools like Next.js 14, Tailwind CSS, and the TMDB API. This project helps you learn how to create a responsive, interactive, and visually stunning movie browsing experience, similar to IMDB." image="/popcornflix1.png" link="https://github.com/adithyasd10/imdb-next" link1="https://popcorn-flix-lake.vercel.app/"/>
+        <Cards title="Savorly(Restaurant App)-Frontend UI" description="A visually appealing and fully responsive restaurant frontend built using Next.js. This modern web interface features smooth animations, engaging image sliders, an intuitive,Add to Cart system, and attractive offer sections —all optimized for desktop and mobile experiences" image="/savorly.jpg" link="https://github.com/adithyasd10/restuarant-next" link1="https://restuarant-next.vercel.app/"/>
+        <Cards title="LunaLog" description="LunaLog is a beautifully designed sleep tracking web application built with Next.js, allowing users to log, visualize, and analyze their sleep habits. It features modern UI design, real-time data insights, and secure authentication with Clerk." image="/sleep-tracker.png" link="https://github.com/adithyasd10/sleep-tracker-next" link1="https://luna-log.vercel.app/"/>
+        <Cards title="Campus Compass" description="Campus Compass is a cross-platform application developed using the Flutter framework, designed to enhance campus management and communication for both teachers and students. The application integrates Firebase for authentication and real-time database functionality, providing a robust and scalable solution for collaborative group management." image="/IV-tracker.jpg" link="https://github.com/adithyasd10/IV-tracker-app" link1="/projects"/>
+      </div>
+        
     </div>
     
   )
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -6,21 +6,28 @@ type CardsProps = {
   title:string
   description:string
   image:string
-  link:string    
+  link:string   
+  link1:string 
 }
 
-const Cards = ({title,description,image,link}: CardsProps) => {
+const Cards = ({title,description,image,link,link1}: CardsProps) => {
   return (
-     <div className='flex flex-col justify-center items-center bg-black p-6 sm:p-8 rounded-3xl m-4 w-full max-w-[700px] px-4 mx-auto'>
+     <div className='flex flex-col justify-center items-center bg-black p-6 sm:p-8 rounded-3xl m-4 w-full max-w-[700px] px-4 mx-auto '>
            <Image src={image} alt={title} width={600} height={400} className='w-full sm:w-[600px] h-auto rounded-md'/>
-           <div className='text-neutral-400 text-center'>____________________________________________________________</div>
+           <hr className='border-none h-[2px] bg-gradient-to-r from-gray-600 via-gray-400 to-gray-600 w-full my-4'/>
            <div>
             <h1 className='flex justify-center items-center p-3 text-xl sm:text-2xl'>{title}</h1>
             <p className='text-gray-400 text-center max-w-[500px]'>{description}</p>
            </div>
-            <Link href={link} className='p-4'>
-                <button className='flex justify-center items-center p-2 bg-gray-400 hover:bg-gray-500 rounded-lg text-black'>view project</button>
+           <div className='flex flex-row justify-center items-center'>
+              <Link href={link} className='p-2 hover:scale-110 hover:shadow-2xl hover:animate-pulse active:scale-95 transition-all duration-300 ease-in-out'>
+                <Image src="/github.png" alt="github" width={70} height={70} className='flex justify-center items-center p-2 rounded-lg text-black'/>
             </Link>  
+            <Link href={link1} className='p-2 hover:scale-110 hover:shadow-2xl hover:animate-pulse active:scale-95 transition-all duration-300 ease-in-out'>
+                <Image src="/vercel.png" alt="vercel" width={80} height={80} className='flex justify-center items-center p-2 rounded-lg text-black'/>
+            </Link> 
+           </div>
+            
         </div>
   )
 }
