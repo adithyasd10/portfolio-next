5f6731a36f0546c570e01ce71cee4173c39b9c2b
Adithya S D <adithyasd@gmail.com>
2025-07-22 13:14:31 +0530

    updated tech-stack with custom logos

--- components/BentoGrid.tsx
+++ components/BentoGrid.tsx
@@ -12,8 +12,9 @@ const BentoGrid = ({width='100%', height='auto',name,image}:BentoGridProps) => {
   return (
     <div className='flex justify-center items-center border border-gray-500 m-2 p-4 rounded-md w-full h-40 hover:scale-90 transition-all duration-500 bg-gradient-to-br from-gray-800 via-gray-900 to-gray-950'
     style={{width,height}}>
-        <div className='flex flex-col justify-center items-center w-full mx-auto max-w-screen h-full relative'>
-            <h1 className='font-bold font-stretch-130% text-gray-400 hover:text-white'>{name}</h1>
+        <div className='flex flex-roe justify-center items-center w-full mx-auto max-w-screen h-full relative gap-4'>
+            <h1 className='font-bold font-stretch-130% text-gray-400 hover:text-white text-2xl md:text-3xl'>{name}</h1>
+            <Image src={image} alt={image} width={60} height={70}></Image>
         </div>
     </div>
   )
