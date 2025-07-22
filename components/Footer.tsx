31be66c3ac989a08d911496c663a9081adcfbef1
Adithya S D <adithyasd@gmail.com>
2025-07-22 20:04:35 +0530

    added social links in the footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
@@ -7,9 +8,15 @@ const Footer = () => {
         <div className='flex flex-col justify-center items-center gap-2'>
             <h1 className='text-gray-400 text-xl lg:text-2xl transition-all duration-400'>Let's Collaborate </h1>
             <div className='flex flex-row gap-4'>
-                <Image src="/githubf.png" alt="" width={40} height={40} className='hover:scale-110 transition-all duration-200'/>
-                <Image src="/gmail.png" alt="" width={40} height={40} className='hover:scale-110 transition-all duration-200'/>
+              <Link href='https://github.com/adithyasd10'>
+                 <Image src="/githubf.png" alt="" width={40} height={40} className='hover:scale-110 transition-all duration-200'/>
+              </Link>
+              <Link href='mailto:adithyasd@gmail.com'>
+                 <Image src="/gmail.png" alt="" width={40} height={40} className='hover:scale-110 transition-all duration-200'/>
+              </Link>
+              <Link href="https://www.linkedin.com/in/adithyasd/">
                 <Image src="/linkedin.png" alt="" width={40} height={40} className='hover:scale-110 transition-all duration-200'/>
+              </Link>    
             </div>   
         </div>
     </section>
