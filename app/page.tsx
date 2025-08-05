96c311d966492399a07536c40212adbb825a8ec6
Adithya S D <adithyasd@gmail.com>
2025-08-05 21:36:27 +0530

    hero section design fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-r from-slate-800 via-slate-900 to-black h-[665]">
+    <div className="bg-gradient-to-r from-slate-800 via-slate-900 to-black h-[650]">
          <Hero/>
     </div>
    
